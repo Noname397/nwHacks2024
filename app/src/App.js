a3c9d42b9378336dac5bdf8f405fd8bc84dcaf07
Nam <namnguyennhat908@gmail.com>
2024-01-20 18:56:47 -0800

    init form submission

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {BrowserRouter as Router,Routes, Route} from "react-router-dom"
 import { HomePage } from './pages/HomePage';
+import { FormSubmission } from './pages/FormSubmission';
 function App() {
   return (
       <Router>
         <Routes>
           <Route path='/' element={<HomePage></HomePage>} />
+          <Route path='/submit-form' element={<FormSubmission></FormSubmission>}/>
         </Routes>
       </Router>
   );
