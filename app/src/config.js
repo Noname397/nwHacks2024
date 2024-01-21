df2e9c782845bf9b69aef6620e86fa9f71c3315a
trungnguyen <trungn21@student.ubc.ca>
2024-01-20 23:51:11 -0800

    Added homepage

--- app/src/config.js
+++ app/src/config.js
@@ -1,3 +1,5 @@
-const config = {
-    API_KEY: 'ee08f27a3amshe4b7703cfae9005p112d7fjsnde80a7a8bc8b'
+const API_KEY = 'f66c5a91ccmsh80157abf4158d12p1e4949jsn8153988c095b'
+
+module.exports = {
+    API_KEY: API_KEY
 }
